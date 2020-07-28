bd014d5f7bb02294120d5886479ae3e74d7ad578
kominfobaubau <kominfobaubau01@gmail.com>
2020-07-28 14:55:38 +0800

    perbaharui database

--- software.sql
+++ software.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
--- https://www.phpmyadmin.net/
+-- version 4.5.1
+-- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 28 Jul 2020 pada 02.32
--- Versi server: 10.4.13-MariaDB
--- Versi PHP: 7.4.7
+-- Generation Time: Jul 28, 2020 at 08:54 AM
+-- Server version: 10.1.13-MariaDB
+-- PHP Version: 5.6.23
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -24,7 +23,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `download`
+-- Table structure for table `download`
 --
 
 CREATE TABLE `download` (
@@ -34,37 +33,71 @@ CREATE TABLE `download` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `download`
+-- Dumping data for table `download`
 --
 
 INSERT INTO `download` (`id`, `nama`, `link`) VALUES
-(4, 'sdf', 'sdf'),
-(11, 'software tumpukan buku aja', 'tumpukan-buku.jpg'),
-(12, 'asdfasdfasf', '123'),
-(13, 'test', '1564'),
-(14, '123546sdfgsdfg', '45345dfg');
+(13, 'test123234123', '1564'),
+(15, 'Kota Baubau', '123tets'),
+(16, 'Kota Baubau', '1231233'),
+(17, 'testing', '123444'),
+(18, '123', '123333'),
+(19, 'hayatul habirun', '123'),
+(20, 'sdf', 'sdfj'),
+(21, '123', '123'),
+(22, 'asdfjk', 'dsfjkfjf');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `aktif` int(1) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `password`, `aktif`, `created_at`) VALUES
+(1, 'admin', 'admin', 1, '2020-07-28 06:01:38');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indeks untuk tabel `download`
+-- Indexes for table `download`
 --
 ALTER TABLE `download`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT untuk tabel yang dibuang
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT untuk tabel `download`
+-- AUTO_INCREMENT for table `download`
 --
 ALTER TABLE `download`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
-COMMIT;
-
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
